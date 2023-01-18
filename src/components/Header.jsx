cbc18e5b84b409bf3ea666aacddd31d3b0dfddc6
juan camilo patiño gaviria <33268683+subreptixio@users.noreply.github.com>
2023-01-18 13:09:56 -0500

    No fue mucho trabajo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 function Header() {
     return(
-            <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">Seguimiento Pacientes {''}
+            <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
+            Seguimiento Pacientes {''} Con Github {''}
             <span className="text-indigo-600">Veterinaria</span>
             </h1>
         )
